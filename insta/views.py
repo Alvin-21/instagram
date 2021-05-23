28af5e95603b8f2ae9f2f0a7c4af1b734aeff68c
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-23 21:33:23 +0300

    Fix django registration bugs.

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .models import Profile, Image, Follow, Stream, Comment, Likes
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required(login_url='/accounts/login/')
 def index(request):
     user = request.user
     images = Stream.objects.filter(user=user)
