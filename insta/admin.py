8d9986e14b21f9840eb32b496e494f0ce56357fc
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-23 08:02:53 +0300

    Register models.

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image, Follow, Stream
 
 # Register your models here.
+
+admin.site.register(Image)
+admin.site.register(Follow)
+admin.site.register(Stream)
