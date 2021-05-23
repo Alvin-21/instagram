c7717cbee4cc384dab6bcc5505959c53d402ede8
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-23 22:44:22 +0300

    Add new_image template for the form.

--- insta/forms.py
+++ insta/forms.py
@@ -3,9 +3,6 @@ from .models import Image
 from cloudinary.models import CloudinaryField
 
 class NewImageForm(forms.ModelForm):
-    pic = forms.ImageField(required=True)
-    caption = forms.CharField(widget=forms.Textarea(attrs={'class': 'input'}), required=True)
-
     class Meta:
         model = Image
-        fields = ('pic', 'caption')
+        exclude = ['posted', 'profile', 'likes']
