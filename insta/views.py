c090dec34786aa982042c349767ac9778c890324
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-23 16:34:29 +0300

    Configure home url.

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from .models import Profile, Image, Follow, Stream, Comment, Likes
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
