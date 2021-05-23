f623735a169f5ace28b4c5d880395d3d8502fcd1
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-23 23:04:02 +0300

    Add view for new image.

--- insta/views.py
+++ insta/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .models import Profile, Image, Follow, Stream, Comment, Likes
 from django.contrib.auth.decorators import login_required
+from .forms import NewImageForm
 
 # Create your views here.
 
@@ -18,3 +19,23 @@ def index(request):
     image_items = Image.objects.filter(id__in=group_ids).all().order_by('-posted')
 
     return render(request, 'index.html', {"image_items": image_items})
+
+
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    user = request.user.id
+
+    if request.method == "POST":
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.cleaned_data.get('image')
+            caption = form.cleaned_data.get('caption')
+        
+        i, created = Image.objects.get_or_create(image=image, caption= caption, user_id=user)
+        i.save()
+        return redirect('index')
+
+    else:
+        form = NewImageForm()
+
+    return render(request, 'new_image.html', {"form": form})
