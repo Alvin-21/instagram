d356813430c7254cb622bc821b39c825a12b936b
Alvin Awuor <alvinawuor8@gmail.com>
2021-05-23 14:17:51 +0300

    Add method for testing creation of Profile instance.

--- insta/tests.py
+++ insta/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Profile, Image
 
 # Create your tests here.
+
+class ProfileTest(TestCase):
+    def tearDown(self):
+        Profile.objects.all().delete()
+        Image.objects.all().delete()
+    
+    def setUp(self):
+        self.john = Profile(first_name='John', last_name='Doe', bio='This is my profile page')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.john, Profile))
+    
+    
